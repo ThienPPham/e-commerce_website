1e4cd49cd6601a4d1462a1b4111e0bdd957b5b9b
ThienPPham <90206572+ThienPPham@users.noreply.github.com>
2024-05-04 22:49:15 +0700

    Setup

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './redux/slides/counterSlide'
+import styled from 'styled-components'
+
+function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  const Button = styled.button({
+    color: 'red',
+  });
+
+  return (
+    <div>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <Button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
