ec7b7ac4f021514f33fa4cce5e3d0813c08e37bc
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-05-21 18:57:46 +0700

    Navbar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import slider1 from '../../assets/images/slider1.jpg'
 import slider2 from '../../assets/images/slider2.jpg'
 import slider3 from '../../assets/images/slider3.jpg'
 import CardComponent from '../../components/CardComponent/CardComponent';
+import NavBarComponent from '../../components/NavBarComponent/NavBarComponent';
 
 const HomePage = () => {
   const arr = ['TV', 'Tu lanh', 'LapTop']
@@ -26,6 +27,7 @@ const HomePage = () => {
           <div style={{ marginTop: '20px', display: 'flex', alignItems: 'center', gap: '20px'}}>
             <CardComponent/>
           </div>
+          <NavBarComponent />
         </div>
     </>
   )
